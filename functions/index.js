3c210de2f01246e427086f99a2df4b8cdfcd7a21
Sarah Allen <sarah@ultrasaurus.com>
2017-10-06 03:05:53 -0700

    make thumbnail works

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
 const functions = require('firebase-functions');
+const gcs = require('@google-cloud/storage')();
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +11,38 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const THUMB_PREFIX = 'thumb_';
+
+exports.doSomething = functions.storage.object().onChange(event => {
+  console.log('doSomething', event);
+
+  if (event.data.resourceState == 'not_exists') return;
+
+  const filePath = event.data.name;
+  const fileDir = path.dirname(filePath);
+  const fileName = path.basename(filePath);
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+  if (fileName.startsWith(THUMB_PREFIX)) return;
+  
+  // // Download file from bucket.
+  const bucket = gcs.bucket(event.data.bucket);
+
+  return bucket.file(filePath).download({
+    destination: tempFilePath
+  }).then(() => {
+    console.log('Image downloaded locally to', tempFilePath);
+    // Generate a thumbnail using ImageMagick.
+    return spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
+  }).then(() => {
+    console.log('Thumbnail created at', tempFilePath);
+
+    // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
+    const thumbFileName = `${THUMB_PREFIX}${fileName}`;
+    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+    // Uploading the thumbnail.
+    return bucket.upload(tempFilePath, {destination: thumbFilePath});
+  // Once the thumbnail has been uploaded delete the local file to free up disk space.
+  }).then(() => fs.unlinkSync(tempFilePath));
+
+});
