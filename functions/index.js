a77c253ebdf05aa9b57f7c7bd8b2c56364722084
Sarah Allen <sarah@ultrasaurus.com>
2017-11-01 05:40:32 +0100

    this works when deployed

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
+var   gs = require('gs');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -12,19 +13,23 @@ const fs = require('fs');
 //  response.send("Hello from Firebase!");
 // });
 
-const THUMB_PREFIX = 'thumb_';
-
 exports.doSomething = functions.storage.object().onChange(event => {
   console.log('doSomething', event);
 
-  if (event.data.resourceState == 'not_exists') return;
+  // ignore delete events
+  if (event.data.resourceState == 'not_exists') return false;
 
   const filePath = event.data.name;
   const fileDir = path.dirname(filePath);
   const fileName = path.basename(filePath);
   const tempFilePath = path.join(os.tmpdir(), fileName);
-  if (fileName.startsWith(THUMB_PREFIX)) return;
+  if (fileName.endsWith('.png')) return false;
+  if (!fileName.endsWith('.pdf')) return false;
+
+  const newName = path.basename(filePath, '.pdf') + '.png';
+  const tempNewPath = path.join(os.tmpdir(), newName);
   
+
   // // Download file from bucket.
   const bucket = gcs.bucket(event.data.bucket);
 
@@ -32,17 +37,45 @@ exports.doSomething = functions.storage.object().onChange(event => {
     destination: tempFilePath
   }).then(() => {
     console.log('Image downloaded locally to', tempFilePath);
-    // Generate a thumbnail using ImageMagick.
-    return spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
-  }).then(() => {
-    console.log('Thumbnail created at', tempFilePath);
 
-    // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
-    const thumbFileName = `${THUMB_PREFIX}${fileName}`;
-    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+    return new Promise(function (resolve, reject) {
+        gs()
+          .batch()
+          .nopause()
+          .option('-r' + 50 * 2)
+          .option('-dDownScaleFactor=2')
+          .executablePath('lambda-ghostscript/bin/./gs')
+          .device('png16m')
+          .output(tempNewPath)
+          .input(tempFilePath)
+          .exec(function (err, stdout, stderr) {
+              if (!err) {
+                console.log('gs executed w/o error');            
+                console.log('stdout',stdout);            
+                console.log('stderr',stderr);            
+                resolve();
+              } else {
+                console.log('gs error:', err);
+                reject(err);
+              }
+          });
+    });
+
+  }).then(() => {
+    console.log('PNG created at', tempNewPath);
+
     // Uploading the thumbnail.
-    return bucket.upload(tempFilePath, {destination: thumbFilePath});
+    return bucket.upload(tempNewPath, {destination: newName});
   // Once the thumbnail has been uploaded delete the local file to free up disk space.
-  }).then(() => fs.unlinkSync(tempFilePath));
+  }).then(() => {
+    fs.unlinkSync(tempNewPath);
+    fs.unlinkSync(tempFilePath);
+  }).catch((err) => {
+    console.log('exception:', err);
+    return err;
+  });
+
+
+
 
 });
